aa8da5f578c972500b28b67c3110c7f412eb8c83
Jed Parsons <jedp@me.com>
2014-06-09 16:11:23 -0700

    show visual feedback when 'sync' message is received

--- dev_apps/sync-thing/js/syncthing.js
+++ dev_apps/sync-thing/js/syncthing.js
@@ -1,1 +1,17 @@
 // App script goes here.
+window.onload = function() {
+  console.log("** sync thing setting up");
+  var syncNowButton = document.getElementById('sync');
+
+  // toolkit/modules/SyncScheduler.jsm will emit a 'sync' system
+  // message when it's time for us to sync.
+  navigator.mozSetMessageHandler('sync', function() {
+    document.getElementById('feedback').innerHTML =
+      "Received sync system message at " + Date.now();
+  });
+
+  syncNowButton.onclick = function() {
+    navigator.syncScheduler.requestSync("ohai");
+  };
+};
+
